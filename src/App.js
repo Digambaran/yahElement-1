92f0842343f7bb219d1419283c461219de2d2ff4
Digambaran <arjunsreekumar.1@gmail.com>
2022-07-21 18:29:50 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Yahelement-1 from './yahElement-1'
+export default function App() {
+  return (
+    <div>
+      <Yahelement-1/>
+    </div>
+  )
+}
---
+++ src/yahElement-1.js
@@ -0,0 +1,8 @@
+import React from 'react'
+// import env from 'env'
+export const Yahelement-1 = () => {
+  return (
+   <p>Hello from yahElement-1 </p>
+  )
+}
+export default Yahelement-1
